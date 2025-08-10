49d2fffb5bb887d9ff0eb6477c72a2973d239b48
Christian Rowlands <christian.rowlands@gmail.com>
2025-08-10 08:31:01 -0400

    Remove the tabs from the site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,32 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://wavedigger.networksurvey.app';
   
   return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/?tab=bssid`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/?tab=location`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/?tab=celltower`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.8,
-    },
   ];
 }
